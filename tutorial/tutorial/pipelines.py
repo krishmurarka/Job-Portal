f810439c911bb3073a09cff1c84c988cfa9117cd
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-09-11 19:16:24 +0530

    updated_pipelines

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -12,29 +12,28 @@ class TutorialPipeline(object):
     def __init__(self):
         self.create_connection()
         self.create_table()
-        # self.process_item()
-        # self.store_db()
+
     def create_connection(self):
         self.conn = mysql.connector.connect(
             host='localhost',
             user='root',
-            passwd='1234',
-            database='jobs'
+            passwd='taranmysql',
+            database='job_portal'
         )
         self.curr = self.conn.cursor()
 
     def create_table(self):
         self.curr.execute("""DROP TABLE IF EXISTS quotes_tb """)
         self.curr.execute("""CREATE TABLE quotes_tb(
-            job_title text, hyperlink text)""")
+            job_title text, hyperlink text , job_image text)""")
 
     def process_item(self, item, spider):
         self.store_db(item)
         return item
 
     def store_db(self, item):
-        print("Taran kya kaam kiye ho ??????")
-        self.curr.execute("""INSERT INTO quotes_tb VALUES(%s,%s)""", (
-            item['job_title'],item['hyperlink']
+        # print("Taran kya kaam kiye ho ??????")
+        self.curr.execute("""INSERT INTO quotes_tb VALUES(%s,%s,%s)""", (
+            item['job_title'], item['hyperlink'], item['job_image']
         ))
         self.conn.commit()
