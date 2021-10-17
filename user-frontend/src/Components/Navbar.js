e6bb078507455de38343cbb94518de8c20b7a894
krishmurarka <murarka.krish@gmail.com>
2021-10-17 19:38:56 +0530

    Added second line of navbar

--- user-frontend/src/Components/Navbar.js
+++ user-frontend/src/Components/Navbar.js
@@ -5,7 +5,9 @@ class Navbar extends Component {
   render() {
     return (
       <>
+        {/* 1st col of Navbar i.e. logo and contact us  */}
         <div>
+        
           <nav className="navbar navbar-light ">
             <div className="container">
               <a className="navbar-brand" href="#">
@@ -17,11 +19,17 @@ class Navbar extends Component {
             </div>
           </nav>
         </div>
-        <nav class="navbar navbar-expand-lg navbar-light ">
-          <div class="container-fluid">
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        {/* 2nd col of Navbar i.e. Employement Type stream  */}
+        <nav class="navbar navbar-expand-lg navbar-light second-navbar">
+          <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                {/* Employment type dropdown */}
                 <li class="nav-item dropdown">
                   <a
                     class="nav-link dropdown-toggle"
@@ -30,9 +38,12 @@ class Navbar extends Component {
                     role="button"
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
+                    style={{color: "BLACK"}}
                   >
                     Employment Type
+                    <span className="gapping"></span>
                   </a>
+
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <li>
                       <a class="dropdown-item" href="#">
@@ -42,6 +53,40 @@ class Navbar extends Component {
                     <li>
                       <a class="dropdown-item" href="#">
                         Intern / Part Time
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                {/* Streams Down Down */}
+                <li class="nav-item dropdown">
+                  
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    style={{color: "BLACK"}}
+                  >
+                    Stream Type
+                    <span className="gapping"></span>
+                  </a>
+
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Science
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                          Commerce
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                          Arts
                       </a>
                     </li>
                   </ul>
