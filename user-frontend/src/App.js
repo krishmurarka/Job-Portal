1afcbd509ab6e739c0a4a233d4f3ebb033c0ca26
krishmurarka <murarka.krish@gmail.com>
2021-10-20 19:39:05 +0530

    Added the card component

--- user-frontend/src/App.js
+++ user-frontend/src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar.js";
 import Card from "./Components/Card.js";
+
 function App() {
   return (
     <div className="App">
       <Navbar className="navbar" />
       <div>
-        <p className="featurehead">Featured listing</p>
+        <p className="featurehead" title="swiggy bangaluru urban">Featured listing</p>
       </div>
       <Card />
     </div>
