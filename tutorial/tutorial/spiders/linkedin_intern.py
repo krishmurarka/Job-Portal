8d71bd4a8f45e57c0f850b76603d0e92c2b85130
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-10-24 11:58:13 +0530

    updated_crawler_linkedin_intern

--- tutorial/tutorial/spiders/linkedin_intern.py
+++ tutorial/tutorial/spiders/linkedin_intern.py
@@ -1,3 +1,4 @@
+from os import link
 import scrapy
 from ..items import TutorialItem
 # xyz=[]
@@ -15,18 +16,32 @@ class tryer(scrapy.Spider):
     name = 'quoutes'
     # start_urls=xyz
     start_urls = ['https://in.linkedin.com/jobs/search?keywords=&location=India&locationId=&geoId=102713980&sortBy=R&f_TPR=&f_JT=I&f_E=1%2C2&position=1&pageNum=0']
+    # start_urls = [
+    #     'https://internshala.com/fresher-jobs/page']
+    # for check
 
     def parse(self, response):
+
+        # linkedin
+        # def parse(self, response):
+        # internshala
+        # redirect - link for internshala jobs
+
         items = TutorialItem()
 
         hyperlinks = response.css(
             "a.base-card__full-link").xpath("@href").extract()
-        job_titles = response.css("span.screen-reader-text::text").extract()
+        job_titles = response.css(
+            "span.screen-reader-text::text").extract()
 
         company_name = response.css(
             "h4.base-search-card__subtitle a::text").extract()
         job_location = response.css(
             "div.base-search-card__metadata span.job-search-card__location::text ").extract()
+
+        # job_image = response.css(
+        #     "div.base-card div.search-entity-media").extract()
+
         for i in range(0, len(hyperlinks)):
             # hyperlink
             hyperlink = hyperlinks[i]
@@ -42,8 +57,21 @@ class tryer(scrapy.Spider):
             jobLocation = job_location[i]
             jobLocation = jobLocation.replace('\n', '')
             jobLocation = jobLocation.strip()
+            # jobImage
+            jobImage = "https://static-exp1.licdn.com/sc/h/9a9u41thxt325ucfh5z8ga4m8"
+            # jobCategory
+            jobCategory = 1
+            # streamId
+            streamId = 'S'
             items['job_title'] = job_title
             items['hyperlink'] = hyperlink
             items['companyName'] = companyName
             items['jobLocation'] = jobLocation
+            items['jobCategory'] = jobCategory
+            items['jobImage'] = jobImage
+            items['streamId'] = streamId
+            # items to be added - stream, intern/fte, productbased/servicetype/startup
             yield items
+
+
+# https://static-exp1.licdn.com/sc/h/9a9u41thxt325ucfh5z8ga4m8
