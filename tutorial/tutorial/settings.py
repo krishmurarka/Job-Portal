2e6ab0ca6da7ff9144c66f52af180ff38a797daa
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-10-24 12:07:41 +0530

    updated_settings

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'tutorial.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
+# USER_AGENT = 'my-cool-project (http://example.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -25,9 +25,9 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 10
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
