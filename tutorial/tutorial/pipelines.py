acb3e2164158174b9b47bca52073108bceba8055
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-10-24 12:07:23 +0530

    updated_pipelines

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -23,17 +23,20 @@ class TutorialPipeline(object):
         self.curr = self.conn.cursor()
 
     def create_table(self):
-        self.curr.execute("""DROP TABLE IF EXISTS quotes_tb """)
-        self.curr.execute("""CREATE TABLE quotes_tb(
-            job_title text, hyperlink text , companyName text , jobLocation text)""")
+        print('a')
+        # self.curr.execute("""DROP TABLE IF EXISTS quotes_tb """)
+        # self.curr.execute("""CREATE TABLE quotes_tb(
+        #        id int, category_id int,company_logo text, company_name text, job_location text, job_title text, stream_id text, url VARCHAR(255), UNIQUE (url))""")
 
     def process_item(self, item, spider):
         self.store_db(item)
         return item
 
     def store_db(self, item):
-        # print("Taran kya kaam kiye ho ??????")
-        self.curr.execute("""INSERT INTO quotes_tb VALUES(%s,%s,%s,%s)""", (
-            item['job_title'], item['hyperlink'], item['companyName'], item['jobLocation']
-        ))
+        # self.curr.execute("""INSERT INTO quotes_tb (job_title,url,company_name,job_location,category_id) VALUES(%s,%s,%s,%s,%d)""", (
+        #     item['job_title'], item['hyperlink'], item['companyName'], item['jobLocation'],item['jobCategory']
+
+        self.curr.execute("""INSERT IGNORE INTO quotes_tb (job_title,url,company_logo,company_name,job_location,category_id,stream_id)VALUES(%s,%s,%s,%s,%s,%s,%s)""", (
+            item['job_title'], item['hyperlink'], item['jobImage'], item['companyName'], item['jobLocation'], item['jobCategory'], item['streamId']))
+
         self.conn.commit()
