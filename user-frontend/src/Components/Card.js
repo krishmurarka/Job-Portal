1abb1a2817483a604ba52e92164e0f4e25062b39
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-10-28 22:28:08 +0530

    updated_Card

--- user-frontend/src/Components/Card.js
+++ user-frontend/src/Components/Card.js
@@ -4,23 +4,56 @@ import AppLogo from "./assets/WebsiteLogo.png";
 
 class Card extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "Full Time",
+      experience: "1 Years",
+      role: "Mid-level"
+    }
+
+    // console.log("hello", props)
+  }
+
+  componentDidMount() {
+    if (this.props.category_id == '1') {
+      console.log(this.props.category_id)
+      this.setState({
+        title: "Intern",
+        experience: "0 Years",
+        role: "Fresher"
+      })
+    } else {
+      this.setState({
+        title: "Full Time",
+        experience: "1 Years",
+        role: "Mid-level"
+      })
+    }
+  }
 
   render() {
+
     return (
+
+
 
       <span className="card-start col-xs-12 col-md-6 col-xl-4">
         <div class="card">
           {/* we need two things in card header (title + image) */}
           <div class="cardheader">
-            <img src={AppLogo} alt="logo" className="Card-logo" />
-            <p className="Company-Name">Swiggy,Bengaluru Urban</p>
+            <img src={this.props.company_logo} alt="logo" className="Card-logo" />
+            <p className="Company-Name">{this.props.company_name},{this.props.job_location}</p>
           </div>
           <div class="card-body">
-            <h5 class="jobTitle">Backend Engineer</h5>
+            <h5 class="jobTitle">{this.props.job_title}</h5>
             <div className="job-type">
-              <p>Full Time &nbsp;|&nbsp;</p>
-              <p> 2-4 Years &nbsp;|&nbsp;</p>
-              <p> Mid-level</p>
+
+              <p> {this.state.title} &nbsp;|&nbsp;</p>
+              <p> {this.state.experience} &nbsp;|&nbsp;</p>
+              <p> {this.state.role}</p>
+
             </div>
             <p class="card-text">
               We are a educational technology provider located in San
@@ -28,7 +61,7 @@ class Card extends Component {
               Fransisco
             </p>
             <div className="buttons">
-              <a href="#" class="btn btn-primary">
+              <a href={this.props.url} class="btn btn-primary">
                 Apply Now
               </a>
               <a href="#" class="btn btn-primary">
