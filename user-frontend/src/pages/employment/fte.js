251fee42e28cf97a6a5fe1c2f1e955e61bf01809
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-10-31 12:37:51 +0530

    updated

---
+++ user-frontend/src/pages/employment/fte.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Fte extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to JobEase</h1>
+                {/* <Navbar className="navbar" /> */}
+            </div>
+        )
+    }
+}
+export default Fte;
---
+++ user-frontend/src/pages/employment/intern.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class intern extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default intern
