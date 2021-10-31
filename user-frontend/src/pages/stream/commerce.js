381d0744db7875b59848082e89105b31f23e4bf1
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-10-31 12:38:51 +0530

    updated_Streams

---
+++ user-frontend/src/pages/stream/commerce.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class commerce extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default commerce
